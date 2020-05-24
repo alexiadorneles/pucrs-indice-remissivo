699f4463d7ceac0aabbd13f5af72dd98c34bbfdb
alexiadorneles <lecavsalexia@gmail.com>
2020-05-24 02:10:20 -0300

    feat(reader): parse book while parsing file.

--- src/util/BookReader.java
+++ src/util/BookReader.java
@@ -1,9 +1,6 @@
 package util;
 
-import domain.Book;
-import domain.Line;
-import domain.Page;
-import domain.Word;
+import domain.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,6 +11,13 @@ import java.nio.file.Paths;
 
 public class BookReader {
     private static final int LINE_LIMITS_TO_A_PAGE = 40;
+    private final DoubleLinkedList<Word> stopWords;
+    private DoubleLinkedList<Word> allWords;
+
+    public BookReader(DoubleLinkedList<Word> stopwords) {
+        this.allWords = new DoubleLinkedList<>();
+        this.stopWords = stopwords;
+    }
 
     public Book read(String fileName) {
         int lineCount = 1;
@@ -26,16 +30,21 @@ public class BookReader {
 
             String lineText = reader.readLine();
             while (lineText != null) {
-                Line line = this.getLine(lineText);
+                Line line = this.getLine(lineText, page);
+                line.setOriginalText(lineText);
                 page.addLine(line);
 
                 if (this.isTheEndOfAPage(lineCount)) {
                     book.addPage(page);
-                    page = new Page(this.calculatePageNumber(lineCount));
+                    page = new Page(this.calculatePageNumber(lineCount) + 1);
                 }
 
                 lineCount++;
                 lineText = reader.readLine();
+            }
+
+            if (!this.isTheEndOfAPage(lineCount)) {
+                book.addPage(page);
             }
 
             return book;
@@ -52,17 +61,40 @@ public class BookReader {
         return lineCount % LINE_LIMITS_TO_A_PAGE == 0;
     }
 
-    private Line getLine(String lineText) {
-        Line line = new Line();
+    private Line getLine(String lineText, Page page) {
+        Line line = new Line(page);
         String[] words = lineText.replaceAll("\\t", " ").split(" ");
         for (String wordText : words) {
-            Word word = new Word(wordText, this.formatText(wordText));
+            String formattedText = this.formatText(wordText);
+
+            if (formattedText.isBlank()) continue;
+
+            Word wordFromList = this.allWords.find(word -> word.getStripedText().equals(formattedText));
+
+            if (wordFromList != null) {
+                wordFromList.incrementNumber();
+                page.incrementNumberOfWords();
+                wordFromList.appearsOn(page);
+                continue;
+            }
+
+            Word word = new Word(wordText, formattedText);
+            if (this.stopWords.contains(word)) {
+                word.setStopword(true);
+                page.incrementNumberOfStopwords();
+                page.incrementNumberOfWords();
+                continue;
+            }
+
+            word.appearsOn(page);
+            page.incrementNumberOfWords();
             line.addWord(word);
+            this.allWords.add(word);
         }
         return line;
     }
 
     private String formatText(String wordText) {
-        return wordText.toLowerCase();
+        return wordText.toLowerCase().replaceAll("[^a-zA-Z0-9 ]", "");
     }
 }
